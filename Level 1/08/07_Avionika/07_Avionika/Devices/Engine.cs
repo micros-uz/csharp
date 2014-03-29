976d6421199cec17a450a20756e4fd0cf26df8bf
micros <www@www.uz>
2014-03-29 13:10:02 +0500

    Added: 8 lesson - inheritance + interfaces

---
+++ Level 1/08/07_Avionika/07_Avionika/Devices/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07_Avionika.Devices
+{
+    internal sealed class Engine : BaseDevice
+    {
+        private string _type;
+
+        public Engine(string type) : base(0)
+        {
+            _type = type;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + " " + _type;
+        }
+
+        //internal void Start()
+        //{
+        //    Console.WriteLine("Engine {0} Started", _type);
+        //}
+
+        //internal void Stop()
+        //{
+        //    Console.WriteLine("Engine {0} Stopped", _type);
+        //}
+    }
+}
---
+++ Level 1/08/08_DemoInterfaces/08_DemoInterfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_DemoInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mas = new string[]{"28052014", "14042014", "01012015"};
+
+            IComparer c = new MyComparer();
+
+            Array.Sort(mas, c);
+
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
